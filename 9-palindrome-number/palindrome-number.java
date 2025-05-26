78d00da0b4118dd5ab3b58b377340a88b306e6e7
sneha <springflowers0606@gmail.com>
2025-05-26 20:02:38 +0530

    Time: 5 ms (70.04%) | Memory: 44.2 MB (37.66%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int revnum = 0;
+        int dup = x; //duplicate variable to store value of x 
+        while(x>0)
+        {
+            int lastdigit = x%10;
+            x = x/10;
+            revnum = (revnum*10)+lastdigit ;
+
+
+        }
+        if(revnum == dup)
+        {
+            return true ;
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+}
+//121 . 121%10 = 1 . store in lastdigit  . and 121/10 = 12 .so x=12 
+
+// revnum = (revnum *10)+lastdigit 
+//0*10 + 1-> 0 + 1 ->1
+//1*10 + 2 --> 10 + 2 -->12
+//12*10 + 1 --> 120 + 1 -->121 is revnum
