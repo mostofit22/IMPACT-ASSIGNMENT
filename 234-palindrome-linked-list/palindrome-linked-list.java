c8f108251909bf924dcfb28ad7f2a4fd60587422
sneha <springflowers0606@gmail.com>
2025-06-04 13:44:10 +0530

    Time: 9 ms (23.74%) | Memory: 62.6 MB (63.73%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; // slow is at middle
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public boolean compare(ListNode head1, ListNode head2) {
+        while (head1 != null && head2 != null) {
+            if (head1.val != head2.val) return false;
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find the middle
+        ListNode mid = middleNode(head);
+
+        // Step 2: Reverse second half
+        ListNode revSecondHalf = reverseList(mid);
+
+        // Step 3: Compare both halves
+        return compare(head, revSecondHalf);
+    }
+}
